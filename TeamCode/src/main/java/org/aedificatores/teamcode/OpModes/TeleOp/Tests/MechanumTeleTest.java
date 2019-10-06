7cd618f67508d18796e78bba171445655230b0cf
Mister-Minister-Master <misterministermaster@gmail.com>
2019-10-06 14:37:30 -0400

    Created Mechanum Tester

---
+++ TeamCode/src/main/java/org/aedificatores/teamcode/OpModes/TeleOp/Tests/MechanumTeleTest.java
@@ -0,0 +1,28 @@
+package org.aedificatores.teamcode.OpModes.TeleOp.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.aedificatores.teamcode.Mechanisms.Drivetrains.Mechanum;
+import org.aedificatores.teamcode.Universal.Math.Vector2;
+
+@TeleOp(name = "Mechanum Tele Test")
+public class MechanumTeleTest extends OpMode {
+    private Mechanum drivetrain;
+
+    double x, y;
+
+
+    @Override
+    public void init() {
+        drivetrain = new Mechanum();
+        drivetrain.initMotors(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        x = .3 * gamepad1.left_stick_x;
+        y = .3 * gamepad1.left_stick_y;
+        drivetrain.setVelocity(new Vector2(x,y));
+    }
+}
