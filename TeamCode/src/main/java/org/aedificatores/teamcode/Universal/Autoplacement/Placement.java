56bac6ec4bf0e839535f71715c856eda69f60f00
mayadapples <tlovinski@outlook.com>
2019-11-10 17:50:44 -0500

    fixed Placement.

--- TeamCode/src/main/java/org/aedificatores/teamcode/Universal/Autoplacement/Placement.java
+++ TeamCode/src/main/java/org/aedificatores/teamcode/Universal/Autoplacement/Placement.java
@@ -1,5 +1,6 @@
 package org.aedificatores.teamcode.Universal.Autoplacement;
 
+import org.aedificatores.teamcode.Universal.Math.Pose3;
 import org.aedificatores.teamcode.Universal.Math.Vector3;
 
 import java.io.BufferedReader;
@@ -9,50 +10,50 @@ import java.util.Stack;
 import java.nio.file.*;
 
 public class Placement {
-	Stack<Vector3> linear = new Stack<Vector3>();
-
-	class Vector3Alias {
-		int x, y;
-		BlockOrientationAlias boa;
-
-		Vector3Alias(Vector3 vector) {
-			x = (int) vector.x;
-			y = (int) vector.y;
-
-			switch ((int) vector.z) {
-				case 1:
-					boa = BlockOrientationAlias.UP;
-					break;
-				case 0:
-					boa = BlockOrientationAlias.DOWN;
-					break;
-				case 3:
-					boa = BlockOrientationAlias.LEFT;
-					break;
-				case 4:
-					boa = BlockOrientationAlias.RIGHT;
-					break;
-				default:
-					boa = BlockOrientationAlias.STUB;
-					break;
-			}
-		}
-	}
-
 	public enum BlockOrientationAlias {
 		UP, DOWN, LEFT, RIGHT, STUB
 	}
 
+	public class BlockPose {
+		BlockOrientationAlias orientation;
+		int x = 0;
+		int y = 0;
+		int z = 0;
+
+		BlockPose(int x, int y, int z, BlockOrientationAlias o) {
+			x = x;
+			y = y;
+			z = z;
+			orientation = o;
+		}
+	}
+
+	Stack<BlockPose> linear = new Stack<BlockPose>();
+
 	Placement(String exchangeString) {
 		String delimiter = "[,]+";
 		String[] tokens = exchangeString.split(delimiter);
-		for (int i = 0; i < tokens.length; i += 3) {
-			// TODO: Does this work without casting, furthermore would it be faster?
-			linear.push(new Vector3(
-					(double) Integer.parseInt(tokens[i]),
-					(double) Integer.parseInt(tokens[i + 1]),
-					(double) Integer.parseInt(tokens[i + 2]))
-			);
+		for (int i = 0; i < tokens.length; i += 4) {
+			BlockOrientationAlias asAlias;
+
+			switch (Integer.parseInt(tokens[i + 3])) {
+				case 0:
+					asAlias = BlockOrientationAlias.DOWN;
+					break;
+				case 1:
+					asAlias = BlockOrientationAlias.UP;
+					break;
+				case 2:
+					asAlias = BlockOrientationAlias.LEFT;
+					break;
+				case 3:
+					asAlias = BlockOrientationAlias.RIGHT;
+					break;
+				default:
+					asAlias = BlockOrientationAlias.STUB;
+			}
+
+			linear.push(new BlockPose(Integer.parseInt(tokens[i]), Integer.parseInt(tokens[i + 1]), Integer.parseInt(tokens[i + 2]), asAlias));
 		}
 	}
 
