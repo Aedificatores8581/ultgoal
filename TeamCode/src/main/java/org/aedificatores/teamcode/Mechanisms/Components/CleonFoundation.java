67ee78e111fce2186b9965ec1920456fe502b3ce
mayadapples <tlovinski@outlook.com>
2020-01-02 14:05:08 -0500

    Created CleonFoundation

---
+++ TeamCode/src/main/java/org/aedificatores/teamcode/Mechanisms/Components/CleonFoundation.java
@@ -0,0 +1,21 @@
+package org.aedificatores.teamcode.Mechanisms.Components;;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class CleonFoundation {
+    private Servo[] servos;
+
+    private static final String HARDWARE_MAP_NAME = "foundation";
+    private final double CLOSED_POSITION = -1.0;
+    private final double OPEN_POSITION = 1.0;
+
+    public CleonFoundation(HardwareMap map) {
+        servos[0] = map.servo.get(HARDWARE_MAP_NAME + "1");
+        servos[1] = map.servo.get(HARDWARE_MAP_NAME + "2");
+    }
+
+    public void setServoPosition(double pos) { servo.setPosition(pos); }
+    public void close() { setServoPosition(CLOSED_POSITION); }
+    public void open() { setServoPosition(OPEN_POSITION); }
+}
