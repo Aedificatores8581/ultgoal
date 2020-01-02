af13ef9307a16e8fb541dd448ccd95df7e1b5001
Mister-Minister-Master <misterministermaster@gmail.com>
2020-01-02 17:27:36 -0500

    stuff theo worked on: fixed grabber

--- TeamCode/src/main/java/org/aedificatores/teamcode/Mechanisms/Components/CleonGrabber.java
+++ TeamCode/src/main/java/org/aedificatores/teamcode/Mechanisms/Components/CleonGrabber.java
@@ -1,32 +1,28 @@
 package org.aedificatores.teamcode.Mechanisms.Components;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class CleonGrabber {
     private Servo pusherServo;
-    private Servo grabberServoFront;
-    private Servo grabberServoBack;
+    private Servo grabberServo;
     private Servo rotationServo;
+    // private CRServo extension;
 
     private static final String HARDWARE_MAP_NAME_PREFIX = "grab";
     private static final String HARDWARE_MAP_NAME_PUSHER = "pushb";
-    private static final String HARDWARE_MAP_NAME_FRONT = "front";
-    private static final String HARDWARE_MAP_NAME_BACK = "back";
     private static final String HARDWARE_MAP_NAME_ROTATION = "grotate";
-    private static final double FRONT_CLOSED = 0.3;
-    private static final double FRONT_OPEN = 0;
-    private static final double BACK_CLOSED = 0.3;
-    private static final double BACK_OPEN = 0;
-    private static final double PUSHER_UP = 0.00;
-    private static final double PUSHER_MIN = 0.45;
-    private static final double ROTATION_RESET = 0.50;
-    private static final double ROTATION_ALT = 0.85;
+    private static final String HARDWARE_MAP_NAME_EXTENSION = "extend";
+    private static final double GRABBER_CLOSED = 0;
+    private static final double GRABBER_OPEN = 0;
+    private static final double PUSHER_RETRACTED = 0;
+    private static final double PUSHER_NORMAL = 0;
+    private static final double ROTATION_FLIPPED = 0.85;
 
     public CleonGrabber(HardwareMap map) {
         pusherServo = map.servo.get(HARDWARE_MAP_NAME_PREFIX + HARDWARE_MAP_NAME_PUSHER);
-        grabberServoFront = map.servo.get(HARDWARE_MAP_NAME_PREFIX + HARDWARE_MAP_NAME_FRONT);
-        grabberServoBack = map.servo.get(HARDWARE_MAP_NAME_PREFIX + HARDWARE_MAP_NAME_BACK);
+        grabberServo = map.servo.get(HARDWARE_MAP_NAME_PREFIX);
         rotationServo = map.servo.get(HARDWARE_MAP_NAME_ROTATION);
     }
 
@@ -34,37 +30,27 @@ public class CleonGrabber {
         servo.setPosition(pos);
     }
 
-    public void close() {
-        setServoPosition(grabberServoBack, BACK_CLOSED);
-        setServoPosition(grabberServoFront, FRONT_CLOSED);
+    public void closeGrabber() {
+        setServoPosition(grabberServo, GRABBER_CLOSED);
     }
 
-    public void open() {
-        setServoPosition(grabberServoBack, BACK_OPEN);
-        setServoPosition(grabberServoFront, FRONT_OPEN);
+    public void openGrabber() {
+        setServoPosition(grabberServo, GRABBER_OPEN);
     }
 
-    public void pusherUp() {
-        setServoPosition(pusherServo, PUSHER_UP);
+    public void retractPusher() {
+        setServoPosition(pusherServo, PUSHER_RETRACTED);
     }
 
-    public void pusherDown(double pos) {
-        if (pos > PUSHER_MIN) {
-            setServoPosition(pusherServo, pos);
-        } else {
-            setServoPosition(pusherServo, PUSHER_MIN);
-        }
+    public void returnPusher() {
+        setServoPosition(pusherServo, PUSHER_NORMAL);
     }
 
-    public void pusherDown() {
-        setServoPosition(pusherServo, PUSHER_MIN);
+    public void rotateGrabber(double rotation) {
+        setServoPosition(rotationServo, rotation);
     }
 
-    public void rotateBack() {
-        setServoPosition(rotationServo, ROTATION_ALT);
-    }
-
-    public void rotateFront() {
-        setServoPosition(rotationServo, ROTATION_RESET);
+    public void flipGrabber() {
+        setServoPosition(rotationServo, ROTATION_FLIPPED);
     }
 }
