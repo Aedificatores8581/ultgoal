7eb4840c54c762cdb79e227a562ec4b7bf13158b
theZebraBatman <fgportman00@gmail.com>
2020-02-19 14:26:57 -0500

    Added temporary manual lift and claw controls on CleonTeleOp

--- TeamCode/src/main/java/org/aedificatores/teamcode/OpModes/TeleOp/CleonBotTeleop.java
+++ TeamCode/src/main/java/org/aedificatores/teamcode/OpModes/TeleOp/CleonBotTeleop.java
@@ -20,8 +20,6 @@ public class CleonBotTeleop extends OpMode {
     final static double SLOW_STRAFE_SPEED = 0.375;
     final static double SLOW_FORWARD_SPEED = 0.3;
 
-    boolean canGrab = true;
-
 
 
     public enum IntakeState{
@@ -47,6 +45,7 @@ public class CleonBotTeleop extends OpMode {
 
     FoundationState foundationState = FoundationState.OPEN;
     boolean canSwitchFoundation = true;
+    boolean canGrab = true;
     @Override
     public void init() {
         try {
@@ -56,7 +55,6 @@ public class CleonBotTeleop extends OpMode {
             telemetry.addLine("\n\nStopping OpMode");
             requestOpModeStop();
         }
-
         leftStick1 = new Vector2();
         rightStick1 = new Vector2();
         leftStick2 = new Vector2();
@@ -147,26 +145,26 @@ public class CleonBotTeleop extends OpMode {
                 break;
         }
 
+
         if(gamepad2.left_bumper)
             robot.lift.idle();
         else if (gamepad2.left_stick_button)
             robot.lift.setLiftPower(-1);
         else if(Math.abs(gamepad2.left_stick_y) > 0.1)
             robot.lift.setNormalizedLiftPower(gamepad2.left_stick_y);
-        else {
+        else {/*
             if (gamepad2.dpad_up)
                 robot.lift.snapToStone(robot.lift.closestBlockHeight + 1);
             else if (gamepad2.dpad_down)
                 robot.lift.snapToStone(robot.lift.closestBlockHeight - 1);
             else
                 robot.lift.snapToStone(robot.lift.closestBlockHeight);
-            robot.lift.setPowerUsinngPID();
+            robot.lift.setPowerUsinngPID();*/
+            if(gamepad2.dpad_up)
+                robot.lift.setNormalizedLiftPower(0.375);
+            else if(gamepad2.dpad_down)
+                robot.lift.setNormalizedLiftPower(-0.375);
         }
-
-        if(gamepad2.right_bumper){
-
-        }
-
 
 
 
@@ -179,6 +177,8 @@ public class CleonBotTeleop extends OpMode {
                     robot.grabber.extendState = CleonGrabber.ExtendState.MOVE;
                     robot.intake.resetIntakeState();
                     robot.grabber.openPusher();
+                    robot.grabber.tuckGrabber();
+                    robot.grabber.rotateGrabber(robot.grabber.ROTATION_NORMAL);
                 }
                 if(!gamepad2.right_bumper)
                     canSwitchExtension = true;
@@ -189,22 +189,32 @@ public class CleonBotTeleop extends OpMode {
                     canSwitchExtension= false;
                     extendoState = ExtendoState.EXTENDING;
                     robot.grabber.extendState = CleonGrabber.ExtendState.MOVE;
-                    robot.grabber.openGrabber();
                     robot.intake.resetIntakeState();
-                    robot.grabber.openPusher();
                 }
                 if(!gamepad2.right_bumper)
                     canSwitchExtension = true;
                 break;
         }
 
-        if(robot.grabber.isExtended && gamepad1.b)
-            robot.grabber.flipGrabber();
+        if(robot.grabber.isExtended && robot.grabber.extending)
+            robot.grabber.rotateGrabber(robot.grabber.ROTATION_FLIPPED_180);
 
-        if(gamepad1.y)
+        if(gamepad2.right_trigger > 0.1) {
+            if (robot.grabber.isExtended && canGrab) {
+                robot.grabber.openGrabber();
+                canGrab = false;
+            }
+            else if(robot.grabber.isRetracted && canGrab) {
+                robot.grabber.closeGrabber();
+            }
+        }
+        if(robot.grabber.isRetracted && !canGrab){
             robot.grabber.openGrabber();
-        else if(gamepad2.a)
-            robot.grabber.closeGrabber();
+            canGrab = true;
+        }
+
+
+
         if(robot.intake.stoneState == CleonIntake.StoneState.INTAKING)
             robot.grabber.closePusher();
         if(robot.intake.stoneState == CleonIntake.StoneState.SEARCHING)
@@ -224,17 +234,12 @@ public class CleonBotTeleop extends OpMode {
         rightStick1 = new Vector2(gamepad1.left_stick_x, gamepad1.left_stick_y);
         leftStick2 = new Vector2(gamepad1.left_stick_x, gamepad1.left_stick_y);
 
+        leftStick1.y *= (1 - leftStick1.x * 0.25);
+
         leftStick1.x = gamepad1.dpad_left ?  -SLOW_STRAFE_SPEED : leftStick1.x;
         leftStick1.x = gamepad1.dpad_right ?  SLOW_STRAFE_SPEED : leftStick1.x;
         leftStick1.y = gamepad1.dpad_up ?  SLOW_FORWARD_SPEED : leftStick1.y;
         leftStick1.y = gamepad1.dpad_down ?  -SLOW_FORWARD_SPEED : leftStick1.y;
     }
 
-    public void extendGrabber(){
-
-    }
-
-    public void dropBlock (){
-        
-    }
 }
