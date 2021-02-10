e357eb26c58f5b5861fd96ce5e61b78d498b935f
hseachrist <misterministermaster@gmail.com>
2021-02-10 17:47:25 -0500

    New Config for Gandalf

---
+++ TeamCode/src/main/java/org/aedificatores/teamcode/Mechanisms/Robots/GandalfBotConfig.java
@@ -0,0 +1,16 @@
+package org.aedificatores.teamcode.Mechanisms.Robots;
+
+public interface GandalfBotConfig {
+    interface DT {
+        String RF = "Front Right";
+        String LF = "Front Left";
+        String RR = "Rear Right";
+        String LR = "Rear Left";
+    }
+
+    interface ODOM {
+        String RIGHT = "Front Right";
+        String LEFT = "Front Left";
+        String STRAFE = "Rear Right";
+    }
+}
